fb64b4985d4b0049fbf1c2ec0c16ba0fbe86d39b
julia <julia.albq@gmail.com>
2023-01-26 13:50:48 -0300

    chore: refactor js to simpler logic with funciton called by the 'onclick'

--- index.js
+++ index.js
@@ -1,89 +1,20 @@
-const allNumbers = Array.from(document.querySelectorAll('.number'))
 const display = document.querySelector('.display')
-const allOperations = Array.from(document.querySelectorAll('.operation'))
-const equalButton = document.querySelector('.equal')
 
-let previousNumber = ''
-let currentNumber = ''
-let operation = ''
-
-function displayNumbers(number) {    
-    if(currentNumber.includes('.') && number == ".") return
-
-    display.innerText = `${currentNumber}${number.toString()}`
+function cleanDisplay() {
+    display.innerText = ""
 }
 
-function updateDisplay(){
-    previousNumber = currentNumber
-    currentNumber = display.innerText
-    console.log(previousNumber, currentNumber)
-}
-    
+function displayValue(value) {
+    if(display.innerText.includes('.') && value == ".") return
 
-function displayOperation(operation){
-
-    // if(this.previousNumber != ""){
-    //     this.displayResult
-    // }
-
-    previousNumber = currentNumber
-    currentNumber = ""
-    operation = operation
-    console.log(operation)
+    display.innerText += value
+    return value
 }
 
 function displayResult() {
-    let result
-    if(previousNumber != ""){
-        const previousNumberFloat = parseFloat(previousNumber)
-        const currentNumberFloat = parseFloat(currentNumber)
+    let operation = display.innerText
+    let result = eval(operation)
 
-        if(isNaN(previousNumberFloat) || isNaN(currentNumberFloat)) return
-
-        switch (operation) {
-            case '+':
-                result = previousNumberFloat + currentNumberFloat
-                break
-            case '-':
-                result = previousNumberFloat - currentNumberFloat
-                break
-            case 'X':
-                result = previousNumberFloat * currentNumberFloat
-                break
-            case '÷':
-                result = previousNumberFloat / currentNumberFloat
-                break
-            default: 
-                return
-        }
-
-        console.log(this.operation) 
-
-        currentNumber = result
-        display.innerText = result
-    }
+    display.innerText = result
+    return result
 }
-
-allNumbers.map(numberButton => {
-    numberButton.addEventListener('click', (event) => {
-        if(event.target == numberButton) {
-            displayNumbers(numberButton.innerText)
-            updateDisplay()
-        }
-    })
-})
-
-allOperations.map(operation => {
-    operation.addEventListener('click', (event) => {
-        if(event.target == operation){
-            // console.log(operation.innerText)
-             displayOperation(operation.innerText)
-             displayResult()
-        }
-    })
-})
-
-equalButton.addEventListener('click', () => {
-    console.log('click')
-    displayResult()
-})
